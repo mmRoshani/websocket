71b55ea7e166f8fdcdce8cd7b8d244a952daf448
Mohammad Mojtaba Roshani-TOMaaR <mohammadmojtabaroshani@outlook.com>
2023-03-28 10:37:05 +0430

    chore

--- server.mjs
+++ server.mjs
@@ -4,6 +4,15 @@ import { SERVER_BINDINGS } from "./common/config/server.bindings.mjs";
 
 const { port } = SERVER_BINDINGS();
 
-createServer((req, res) => {}).listen(port, () =>
-  console.log(`Server starts listening on port: ${port}`)
+createServer((req, res) => {
+  res.writeHead(200);
+  res.end("WS server");
+}).listen(port, () => {
+  console.log(`Server starts listening on port: ${port}`);
+});
+
+["uncaughtException", "unhandledRejection"].forEach((event) =>
+  process.on(event, (err) =>
+    console.log(`event: ${event} with message ${err.stack || err}`)
+  )
 );
